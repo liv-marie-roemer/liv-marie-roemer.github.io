8bfcc7d0770691690b05acd76ee12d9b453d7210
Jan Sindler <jansindl3r@gmail.com>
2026-01-21 13:00:29 +0100

    github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://liv-marie-romer.github.io',
   devToolbar: {
     enabled: false,
   },
